171d269a6562db232557c4eda49eb239d940f845
sarkaw salar <sarkaw.salar55@gmail.com>
2022-04-05 23:19:26 +0300

    adding authentication for rooms

--- routes/room.js
+++ routes/room.js
@@ -3,13 +3,15 @@ const express = require("express")
 const Router = express.Router();
 
 const roomController = require("../controller/room.js")
+const auth = require('../middleware/auth')
+
 
 Router.get('/',roomController.getAll);
 Router.get('/room/show/:id', roomController.show)
-Router.get('/room/edit/:id', roomController.edit)
-Router.post('/room/update/:id', roomController.update)
-Router.post('/room/delete/:id', roomController.delete)
-Router.get('/room/create', roomController.create)
-Router.post('/room/store', roomController.store)
+Router.get('/room/edit/:id', auth, roomController.edit)
+Router.post('/room/update/:id', auth, roomController.update)
+Router.post('/room/delete/:id', auth, roomController.delete)
+Router.get('/room/create', auth, roomController.create)
+Router.post('/room/store', auth, roomController.store)
 
 module.exports = Router;
