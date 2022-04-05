b5a933ca79eeaa714490e663f6493076228f6e0f
sarkaw salar <sarkaw.salar55@gmail.com>
2022-04-05 14:33:21 +0300

    adding middleware

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,15 @@ const express = require('express')
 const Router = express.Router();
 
 const authController = require('../controller/auth')
+const guest = require('../middleware/guest')
+const auth = require('../middleware/auth')
 
-Router.get('/login', authController.loginPage);
-Router.post('/login', authController.login);
+Router.get('/login', guest, authController.loginPage);
+Router.post('/login', guest, authController.login);
 
-Router.get('/register', authController.registerPage);
-Router.post('/register', authController.register);
+Router.get('/register', guest, authController.registerPage);
+Router.post('/register', guest, authController.register);
 
-Router.post('/logout', authController.logout);
+Router.post('/logout', auth, authController.logout);
 
 module.exports = Router;
