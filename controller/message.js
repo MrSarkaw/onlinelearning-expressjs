370c8d705f41e2ae6d8c2d5c7ec4346a6a37538a
sarkaw salar <sarkaw.salar55@gmail.com>
2022-04-09 02:06:04 +0300

    delete message

--- controller/message.js
+++ controller/message.js
@@ -1,5 +1,7 @@
 const Room = require('../models/room')
 const Message = require('../models/message')
+
+
 exports.store=async (req, res, next)=>{
     let check = await Room.findByPk(req.body.roomid)
 
@@ -14,3 +16,21 @@ exports.store=async (req, res, next)=>{
     
     return res.redirect('/room/show/'+check.id);
 };
+
+
+exports.destroy = async (req, res, next)=>{
+  Message.findOne({
+       where:{
+           id:req.params.id,
+           userId : req.session.user['id']
+       },
+       include:[{model:Room}]
+   }).then((message)=>{
+       message.destroy();
+
+       return res.redirect('/room/show/'+message.room.id+'');
+    }).catch(()=>{
+       return res.redirect('/');
+   });
+   
+}
--- routes/message.js
+++ routes/message.js
@@ -6,6 +6,6 @@ const messageController = require('../controller/message')
 const auth = require('../middleware/auth')
 
 Router.post('/store/message', auth, messageController.store);
-
+Router.get('/message/delete/:id', auth, messageController.destroy)
 
 module.exports = Router;
