afd16c5743844adf792fae1cdf6861bc9682dcdb
sarkaw salar <sarkaw.salar55@gmail.com>
2022-04-09 13:45:40 +0300

    messages in user profile

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,8 @@
 const User = require('../models/user')
 const Room = require('../models/room')
 const Topic = require('../models/topic')
+const Message = require('../models/message')
+
 exports.show = async (req, res, next)=>{
     const user =await User.findByPk(req.params.id,{
         include:[
@@ -8,21 +10,35 @@ exports.show = async (req, res, next)=>{
                 model:Room,
                 include:[
                     {model:User},
-                    {model:Topic}
+                    {model:Topic},
                 ]
+            },
+            {
+                model:Message,
+                include:[
+                    {model:User},
+                    {model:Room}
+                ],
+                separate : true,
+                limit:10
             }
         ],
         order:[[Room,'id', 'desc']]
     });
     if(user){
+
         const rooms = user.rooms;
+        const messages = user.messages;
+
         const topic = await Topic.findAll({
             include:[{
                 model:Room
             }]
         });
+
+      
     
-        return res.render('profile/show', {profile:user, rooms:rooms, topic:topic});
+        return res.render('profile/show', {profile:user, rooms:rooms, topic:topic, messages:messages});
     }else{
         return res.redirect('/')
     }
