ba51ff302a52a5cf5491fd9c5227e17da6c000f5
sarkaw salar <sarkaw.salar55@gmail.com>
2022-04-14 01:36:48 +0300

    existing email

--- validation/auth/register.js
+++ validation/auth/register.js
@@ -1,8 +1,14 @@
 const {body} = require('express-validator')
-
-module.exports = [
+const User  = require('../../models/user')
+module.exports =  [
     body('name').notEmpty().withMessage('name is required'),
-    body('email').isEmail().withMessage('email is required'),
+    body('email').isEmail().withMessage('email is required').custom(async(value)=>{
+       await User.findOne({where:{email:value}}).then((user)=>{
+           if(user){
+               return Promise.reject('email already has been taken')
+           }
+        })
+    }),
     body('password').isLength({min:6}).withMessage('the password must contain 6 character'),
     body('password_confirmation').custom((value,{req}) =>{
         if(value !== req.body.password){
